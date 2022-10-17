a03832964c1c50d7fb9224723b1b18783cd1c7d3
JulianPetersen <julianpetersen92@gmail.com>
2022-10-17 11:41:03 -0300

    configuracion de entonro remoto para imagenes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,15 @@
+// //Desarrollo
+// export default {
+//     SECRET: 'stock-Api',
+//     host: 'http://localhost',
+//     port: '4000'
+// }
+
+
+//Produccion
+
 export default {
     SECRET: 'stock-Api',
-    host: 'http://localhost',
+    host: 'https://frisor.store/',
     port: '4000'
 }
-
