687f91f940c12558dfe930d8e49103444111078d
JulianPetersen <91999222+JulianPetersen@users.noreply.github.com>
2022-11-01 11:02:38 -0300

    seguimos intentando arreglar el tema de las imagenes, ahora probamos con ip y puerto 4000 del servidor

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
-//Desarrollo
-export default {
-    SECRET: 'stock-Api',
-    host: 'http://localhost',
-    port: '4000'
-}
-
-
-//Produccion
-
+// //Desarrollo
 // export default {
 //     SECRET: 'stock-Api',
-//     host: '179.43.126.181',
+//     host: 'http://localhost',
 //     port: '4000'
 // }
+
+
+// Produccion
+
+export default {
+    SECRET: 'stock-Api',
+    host: '179.43.126.181',
+    port: '4000'
+}
 
 // export default {
 //     SECRET: 'stock-Api',
